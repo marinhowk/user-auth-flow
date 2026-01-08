f7aef5194207cf2fdf6db6564311f689d89272b7
João Marinho <contato.marinhowork@gmail.com>
2026-01-08 00:08:05 -0300

    Realizada correção de erros

--- main.py
+++ main.py
@@ -6,12 +6,18 @@ from services.email_service import enviar_email
 db = BancoDeDados()
 repo = UserRespository(db)
 
+def titulo(texto):
+    print("=" * 50)
+    print((texto).center(50))
+    print("=" * 50)
+
 def menu_principal():
     print("[ 1 ] Realizar Login")
     print("[ 2 ] Realizar Cadastro")
     print("[ 3 ] Redefinir senha")
 
 def login():
+    titulo("Realize o seu login")
     email = input("Email: ")
     senha = input("Senha: ")
 
@@ -22,39 +28,37 @@ def login():
         login()
 
 def cadastro():
+    titulo("Realize seu cadastro")
     nome = input("Nome: ")
     email = input("E-mail: ")
     senha = input("Senha: ")
     confirmar_senha = input("Confirme a sua senha: ")
+
+    if not nome or not email or not senha or not confirmar_senha:
+        print("Prencha todos os campos.")
+        return cadastro()
+
+    if senha != confirmar_senha:
+        print("As senhas são divergentes.")
+        return cadastro()
 
     usuario = Users(nome, email, senha)
     tentativas = 3
 
     codigo_gerado = enviar_email(email)
 
-    if nome and email and senha and confirmar_senha:
-        if confirmar_senha == senha:
+    while tentativas > 0:
+        codigo_digitado = input("Insira o código de confirmação enviado no email: ")
 
-            while tentativas > 0:
-                codigo_digitado = input("Insira o código de confirmação enviado no email: ")
-
-                if codigo_digitado == str(codigo_gerado):
-                    repo.criar_usuario(usuario)
-                    login()
-                    return
-                else:
-                    tentativas -= 1
-                    print(f"Código inválido. Tentativas restantes: {tentativas}")
-
-            print("Número máximo de tentativas atingido. Código expirado.")
-
+        if codigo_digitado == str(codigo_gerado):
+            repo.criar_usuario(usuario)
+            login()
+            return
         else:
-            print("As senhas são divergentes, insira das informações novamente.")
-            cadastro()
+            tentativas -= 1
+            print(f"Código inválido. Tentativas restantes: {tentativas}")
 
-    else:
-        print("Preencha todos os campos.")
-        cadastro()
+    print("Número máximo de tentativas atingido. Código expirado.")
 
 def redefinir_senha():
     pass
