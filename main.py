85009385dece5a2016252e706a5f4f806c4fecd1
João Marinho <contato.marinhowork@gmail.com>
2026-01-07 23:53:52 -0300

    Realizando transição de interatividade do usuario para o terminal

--- main.py
+++ main.py
@@ -1,101 +1,86 @@
-import streamlit as st
 from database.database import BancoDeDados
 from database.repositories import UserRespository
 from models.models import Users
-from services.email_service import enviar_email, codigo_de_confirmacao
+from services.email_service import enviar_email
 
 db = BancoDeDados()
 repo = UserRespository(db)
 
-# --- CONFIGURAÇÃO DA PÁGINA ---
-st.set_page_config(page_title="User Auth Flow", layout="wide")
+def menu_principal():
+    print("[ 1 ] Realizar Login")
+    print("[ 2 ] Realizar Cadastro")
+    print("[ 3 ] Redefinir senha")
 
-# --- CONTROLE DE NAVEGAÇÃO
-if "tela" not in st.session_state:
-    st.session_state.tela = "login"
+def login():
+    email = input("Email: ")
+    senha = input("Senha: ")
 
-# --- CONTROLE DE SESSÃO DAS PÁGINAS ---
-def tela_de_login():
-    st.session_state.tela = "login"
+    if repo.login(email, senha):
+        pagina_inicial()
+    else:
+        print("Email ou senha inválidos.")
+        login()
 
-def tela_de_cadastro():
-    st.session_state.tela = "cadastro"
+def cadastro():
+    nome = input("Nome: ")
+    email = input("E-mail: ")
+    senha = input("Senha: ")
+    confirmar_senha = input("Confirme a sua senha: ")
 
-def tela_codigo_de_confirmacao():
-    st.session_state.tela = "codigo"
+    usuario = Users(nome, email, senha)
+    tentativas = 3
 
-def pagina_principal():
-    st.session_state.tela = "pagina_principal"
+    codigo_gerado = enviar_email(email)
 
-# --- TELA DE LOGIN ---
-if st.session_state.tela == "login":
-    col1, col2, col3 = st.columns([1, 1, 1])
+    if nome and email and senha and confirmar_senha:
+        if confirmar_senha == senha:
 
-    with col2:
-        st.title("Olá!")
-        st.subheader("Seja bem vindo de volta!")
+            while tentativas > 0:
+                codigo_digitado = input("Insira o código de confirmação enviado no email: ")
 
-        msg_login = st.empty()
-
-        email = st.text_input("Email")
-        senha = st.text_input("Password", type="password")
-
-        col_cadastro, col_entrar = st.columns([1, 1])
-
-        with col_cadastro:
-            if st.button("Criar Conta", use_container_width= True):
-                st.session_state.tela = "cadastro"
-                st.rerun()
-
-        with col_entrar:
-            if st.button("Entrar", use_container_width= True):
-                if repo.login(email, senha):
-                    st.session_state.tela = "pagina_principal"
-                    st.rerun()
+                if codigo_digitado == str(codigo_gerado):
+                    repo.criar_usuario(usuario)
+                    login()
+                    return
                 else:
-                    msg_login.error("Email ou senha inválidos")
+                    tentativas -= 1
+                    print(f"Código inválido. Tentativas restantes: {tentativas}")
 
-        st.divider()
+            print("Número máximo de tentativas atingido. Código expirado.")
 
-        st.button("Esqueceu a senha? Clique aqui", use_container_width= True)
+        else:
+            print("As senhas são divergentes, insira das informações novamente.")
+            cadastro()
 
-# --- TELA DE CADASTRO ---
-elif st.session_state.tela == "cadastro":
-    col1, col2, col3 = st.columns([1, 1, 1])
+    else:
+        print("Preencha todos os campos.")
+        cadastro()
 
-    msg = st.empty()
+def redefinir_senha():
+    pass
 
-    with col2:
-        st.title("Crie uma nova conta")
-        nome_cadastro = st.text_input("Nome")
-        email_cadastro = (st.text_input("E-mail"))
-        senha_cadastro = st.text_input("Senha", type="password")
-        confirmacao_de_senha = st.text_input("Confirme a senha", type= "password")
+def pagina_inicial():
+    print("Bem Vindo.")
 
-        if st.button("Registrar", use_container_width= True):
-            if senha_cadastro == confirmacao_de_senha:
-                enviar_email(email_cadastro)
-                st.session_state.tela = "codigo"
-                st.rerun()
-            else:
-                msg.error("A senha de confirmação diverge da senha informada")
+def escolha_menu():
+    try:
+        opcao_escolhida = int(input("Escolha uma opção: "))
 
-# --- TELA DE CONFIRMAÇÃO DE EMAIL ---
-elif st.session_state.tela == "codigo":
-    col1, col2, col3 = st.columns([1, 1, 1])
+        match opcao_escolhida:
+            case 1:
+                login()
+            case 2:
+                cadastro()
+            case 3:
+                redefinir_senha()
+            case _:
+                print("Opção inválida")
+    except:
+        escolha_menu()
 
-    with col2:
-        msg = st.empty()
-        st.subheader(f"Insira o código de verificação enviado no email informado")
-        codigo_de_verificação = st.number_input("Codigo de Verificação", max_value= 9999)
+def main():
+    menu_principal()
+    escolha_menu()
 
-        if st.button("Confirmar", use_container_width= True):
-            if codigo_de_verificação == codigo_de_confirmacao():
-                st.session_state.tela = "login"
-                st.rerun()
-            else:
-                msg.error("Código inválido")
-
-# --- TELA PAGINA PRINCIPAL ---
-elif st.session_state.tela == "pagina_principal":
-    st.text("BemVindo.")
+if __name__ == "__main__":
+    main()
--- services/email_service.py
+++ services/email_service.py
@@ -9,14 +9,19 @@ load_dotenv()
 EMAIL_ADRESS = os.getenv("EMAIL_ADDRESS")
 EMAIL_PASSWORD = os.getenv(("EMAIL_PASSWORD"))
 
-def codigo_de_confirmacao():
-    codigo = random.randint(1000, 9999)
-    return codigo
+def enviar_email(destinatario):
+    codigo_gerado = random.randint(1000, 9999)
 
-def enviar_email (destinatario):
+    assunto = "Confirmação de Email"
+
+    corpo = f"""
+        <p>Prezado</p>
+        <p>Segue código de verificação <strong>{codigo_gerado}</strong></p>
+    """
+
     msg = email.message.Message()
     msg["Subject"] = assunto
-    msg["From"] =  EMAIL_ADRESS
+    msg["From"] = EMAIL_ADRESS
     msg["To"] = destinatario
     msg.add_header("Content-Type", "text/html")
     msg.set_payload(corpo)
@@ -26,13 +31,5 @@ def enviar_email (destinatario):
         s.login(EMAIL_ADRESS, EMAIL_PASSWORD)
         s.sendmail(EMAIL_ADRESS, [destinatario], msg.as_string().encode("utf-8"))
 
-assunto = "Confirmação de Email"
-
-corpo = f"""
-<p>Prezado</p>
-<p>Segue código de verificação {codigo_de_confirmacao()}</p>"""
-
-
-
-
+    return codigo_gerado
 
